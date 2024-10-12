15315c33474c44cf9fe0050571d3b7b9fb3effa8
Jishen Lin <minmuslin@outlook.com>
2024-10-12 12:52:20 -0700

    +

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by JishenLin on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
